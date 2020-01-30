bbf2d9cffb26080b3dd8751ec7b650a6962ad601
jarokaz <jarokaz@no-reply.com>
2020-01-30 08:43:34 +0000

    Updates

--- lab-04-tfx-cicd/build_pipeline.sh
+++ lab-04-tfx-cicd/build_pipeline.sh
@@ -24,7 +24,7 @@ _TRAINED_MODEL_URI=gs://miniworkshop-artifact-store/trained_models,\
 _DATA_ROOT_URI=gs://workshop-datasets/covertype/full,\
 _PIPELINE_FOLDER=pipeline-dsl,\
 _PIPELINE_DSL=pipeline_dsl.py,\
-_KFP_INVERSE_PROXY_HOST=2234cc1c12c20381-dot-us-central1.notebooks.googleusercontent.com,\
+_KFP_INVERSE_PROXY_HOST=5672d5e5849a82b7-dot-us-central1.notebooks.googleusercontent.com,\
 TAG_NAME=test
 
 gcloud builds submit . --config cloudbuild.yaml --substitutions $SUBSTITUTIONS
