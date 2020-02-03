794b9a96a8fb61fbcbc53cefa34baa3dfab6c75d
Jarek Kazmierczak <jarokaz@hotmail.com>
2020-02-02 21:39:25 -0800

    Update install.sh

--- lab-00-environment-setup-automated/install.sh
+++ lab-00-environment-setup-automated/install.sh
@@ -67,7 +67,7 @@ echo INFO: Required services enabled
 # Give Cloud Build service account the project editor role
 echo INFO:Assigning the Cloud Build service account to the project editor role
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
-CLOUD_BUILD_SERVICE_ACCOUNT="${PROJECT_NUMBER}@cloudbuild.iam.gserviceaccount.com"
+CLOUD_BUILD_SERVICE_ACCOUNT="${PROJECT_NUMBER}@@cloudbuild.gserviceaccount.com"
 gcloud projects add-iam-policy-binding $PROJECT_ID \
   --member serviceAccount:$CLOUD_BUILD_SERVICE_ACCOUNT \
   --role roles/editor
